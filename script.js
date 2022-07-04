ea62c103f2ab7a0edaed072e1673a3be6a20e8c3
hpharis258 <60554302+hpharis258@users.noreply.github.com>
2022-07-05 00:57:58 +0300

    Started Implementing Routing

--- script.js
+++ script.js
@@ -18,76 +18,23 @@ function modeSwitch() {
      );
  }
 
- let routes = {};
- let templates = {};
- let app = document.getElementById("application");
+ const routes = [
+    {
+      path: '/',
+      template: '<h1>Home</h1>'
+    },
+    {
+      path: '/about',
+      template: '<h1>About</h1>',
+    },
+    {
+      path: '/contact',
+      template: '<h1>Contact</h1>',
+    },
+  ];
 
- function home() {
-    let app_div = document.getElementById("application");
-    let div = document.createElement('div');
-    let link = document.createElement('a');
-    link.href = '#/home';
-    link.innerText = 'home';
-
-    div.innerHTML = '<h1>Home</h1>';
-    div.appendChild(link);
-
-    app_div.appendChild(div);
-};
-
-function about() {
-    let app_div = document.getElementById("application");
-    let div = document.createElement('div');
-    let link = document.createElement('a');
-    link.href = '#/about';
-    link.innerText = 'about';
-
-    div.innerHTML = '<h1>About</h1>';
-    div.appendChild(link);
-
-    app_div.appendChild(div);
-};
-
-function route (path, template) {
-    if (typeof template === 'function') {
-        return routes[path] = template;
-    }
-    else if (typeof template === 'string') {
-        return routes[path] = templates[template];
-    } else {
-        return;
-    };
-};
-
-function template(name, templateFunction)
+function home()
 {
-    return templates[name] = templateFunction;
+    page = document.getElementById("wholePage");
+    
 }
-
-template('home', function(){
-    home();
-});
-
-template('about', function(){
-    about();
-});
-
-route('/', 'home');
-route('/about', 'about');
-
-function resolveRoute(route) {
-    try {
-        return routes[route];
-    } catch (e) {
-        throw new Error(`Route ${route} not found`);
-    };
-};
-
-function router(evt) {
-    let url = window.location.hash.slice(1) || '/';
-    let route = resolveRoute(url);
-
-    route();
-};
-window.addEventListener('load', router);
-window.addEventListener('hashchange', router);
