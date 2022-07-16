fc2dafa027faf7585319c329701685b484ae9bcc
hpharis258 <60554302+hpharis258@users.noreply.github.com>
2022-07-16 22:19:34 +0300

    Added VANTA JS
    Added Vanta Js, Background and changed buttons to Links and Styled them.

--- script.js
+++ script.js
@@ -17,24 +17,9 @@ function modeSwitch() {
          }
      );
  }
-
- const routes = [
-    {
-      path: '/',
-      template: '<h1>Home</h1>'
-    },
-    {
-      path: '/about',
-      template: '<h1>About</h1>',
-    },
-    {
-      path: '/contact',
-      template: '<h1>Contact</h1>',
-    },
-  ];
-
 function home()
 {
     page = document.getElementById("wholePage");
     
 }
+
