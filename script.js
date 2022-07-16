00b7fe2e0d0c521707ca05090df6a7f7092471e1
hpharis258 <60554302+hpharis258@users.noreply.github.com>
2022-07-16 23:27:07 +0300

    Added Globe

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function modeSwitch() {
-    var element = document.body;
+    let element = document.body;
     element.classList.toggle("dark-mode");
     element.classList.toggle("#navBar")
+  
  }
 
 
