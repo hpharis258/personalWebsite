edfd77e8e05cd036aafaa3a2d492a27b63b16447
Haroldas <hpharis258@gmail.com>
2023-03-28 23:06:06 +0100

    added stuff

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/haroldasvaranauskas.com/',
+  base: 'https://haroldasvaranauskas.com/',
   plugins: [react()],
 })
